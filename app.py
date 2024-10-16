4b6068d95119c114646b7eab863ddfe543ca3c51
JS1936 <102188643+JS1936@users.noreply.github.com>
2024-10-15 19:58:36 -0700

    Update app.py - revert to 30542ff

--- app.py
+++ app.py
@@ -15,8 +15,6 @@ import shutil
 import io
 from io import BytesIO
 
-from PIL import Image
-
 # from <helper> import <helper_method>
 
 
@@ -79,9 +77,6 @@ def upload_file():
         with open(local_file_path, "rb") as data:
             blob_client.upload_blob(data)
 
-        # Try to get most recent submission 
-        #image = get_most_recent_blob_photo() # temporarily comment out
-
         return f"Photo uploaded successfully: {file.filename}"
 
 @app.route('/download', methods=['GET'])
@@ -116,71 +111,6 @@ def download_file():
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-def get_most_recent_blob_photo():
-
-    # Connection string to Azure Blob Storage
-    connection_string = "DefaultEndpointsProtocol=https;AccountName=capstonestorage123;AccountKey=hcpBIufXBfBnGOxPZJlP7TrySHWBvOY8JvdCQLWjh1kUHQHK38HVNCqnU87JeMO0aQrN6zCLZR5n+AStX2g46g==;EndpointSuffix=core.windows.net"
-
-    # Create the BlobServiceClient object
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-
-    # Specify container and blob (file) name
-    container_name = "mycontainer"
-
-    # Initialize BlobServiceClient
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-
-    # Get container client
-    container_client = blob_service_client.get_container_client(container_name)
-
-    # List all blobs and sort them by last_modified (creation time)
-    blobs = container_client.list_blobs()
-    recent_blob = max(blobs, key=lambda b: b.last_modified)
-
-    # Get the most recent blob name
-    print(f"Most recent blob: {recent_blob.name}")
-
-    # Get the BlobClient for the most recent blob
-    blob_client = container_client.get_blob_client(recent_blob)
-
-    # Download the blob to memory (as a BytesIO object)
-    blob_data = blob_client.download_blob().readall()
-    blob_stream = io.BytesIO(blob_data)
-
-    #print(f"Most recent blob '{recent_blob.name}' is ready for in-memory use.")
-
-    # Now use blob_stream as if it's a file in-memory, e.g., for image processing, etc.
-    # Example of how you might pass the in-memory file for further processing:
-    # Suppose this is an image:
-    image = Image.open(blob_stream)
-    image.show() # It actually shows up!
-
-    return image
-    
-
-
-  
-
-# process the image here...
-
-#string all_blobs = ""
-#for blob in container.list_blobs():
-#    print(f'{blob.name} : {blob.last_modified}')
-    #all_blobs += " | " + blob.name + ":" + blob.last_modified
-
-#return all_blobs
-
-# Download the blob to a local file
-# with open("downloaded_example_photo.png", "wb") as download_file:
-#     download_file.write(blob_client.download_blob().readall())
-#
-#return f"Photo downloaded successfully!"
-
-# THIS ONE:
-# Download the blob to memory (as a BytesIO object)
-#blob_data = blob_client.download_blob().readall()
-#download_stream = io.BytesIO(blob_data)
 
 #file_copy = shutil(file, os.path.dirname(os.path.abspath(__file__)) + file.filename)
 #file_path =  "/Users/jenniferstibbins/PycharmProjects/example/testHoughCircles/Embossed_Braille_subsection_5to10cells.png"
