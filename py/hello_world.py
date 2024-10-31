af1e45933b0bbdbfa6483c4c604b6fed130378db
JS1936 <102188643+JS1936@users.noreply.github.com>
2024-10-30 18:44:43 -0700

    Add py files - hello_world, cellsToText_practice
    Note: Also add __pycache__

---
+++ py/hello_world.py
@@ -0,0 +1,50 @@
+# hello_world.py
+
+# def main():
+#     print("Hello, World!")
+
+# if __name__ == "__main__":
+#     main()
+
+
+import sys
+from PIL import Image
+from cellsToText_practice import cellsToText
+
+import json
+
+
+
+def process_cells(cells):
+    """
+    Processes the list of cells.
+    
+    Args:
+        cells (list): A list of cell values.
+    """
+    print("Processing Cells:")
+    for cell in cells:
+        print(cell)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python hello_world.py <cells_json>")
+        sys.exit(1)
+
+
+    print("Welcome to hello_world.py")
+    # Get the JSON string from the command line argument
+    cells_json = sys.argv[1]
+    
+    # Convert the JSON string back to a list
+    cells = json.loads(cells_json)
+
+    # Process the cells
+    process_cells(cells)
+
+    #######
+
+    cellsToText(cells)
+
+    # Call cellsToText
+    #cellsToText(cells)
